d4b66127291f742ccdc57e5e58b1c0e1a8d984b1
Nathaniel Johnson <nathanjohnson320@gmail.com>
2013-09-05 13:36:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+node_arm
+========
+### Installing node for ARM (Tested on Raspberry Pi Model B Raspbian Wheezy)
+```
+wget http://insrails.herokuapp.com/node_latest_armhf.deb
+sudo dpkg -i node_latest_armhf.deb
+```
+
+Other versions are available:
+- node_0.10.17-1_armhf.deb
+- node_0.10.13-1_armhf.deb
+
+Just replace "node_latest_armhf.deb" with one of these names.
+
---
+++ web.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app).listen(process.env.PORT || 9090);
+console.log("Listening on port " + (process.env.PORT || "9090"));
+
+// Routes
+app.get('/node_latest_armhf.deb', function (req, res) {
+  res.download(__dirname + "/files/node_0.10.18-1_armhf.deb");
+});
+
+app.get('/node_0.10.17-1_armhf.deb', function (req, res) {
+  res.download(__dirname + "/files/node_0.10.17-1_armhf.deb");
+});
+
+app.get('/node_0.10.13-1_armhf.deb', function (req, res) {
+  res.download(__dirname + "/files/node_0.10.13-1_armhf.deb");
+});
