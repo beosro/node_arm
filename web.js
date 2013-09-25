6925f451924a866683a9af499993d066b9607bf1
nathanjohnson320 <nathanjohnson320@gmail.com>
2013-09-25 03:10:58 +0000

    Updated node to v0.10.19

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ console.log("Listening on port " + (process.env.PORT || "9090"));
 
 // Routes
 app.get('/node_latest_armhf.deb', function (req, res) {
+  res.download(__dirname + "/files/node_0.10.19-1_armhf.deb");
+});
+
+app.get('/node_0.10.18-1_armhf.deb', function (req, res) {
   res.download(__dirname + "/files/node_0.10.18-1_armhf.deb");
 });
 
