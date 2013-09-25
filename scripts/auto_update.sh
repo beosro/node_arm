115900d8db83ec1d502a0551d97c7d8936f91f72
nathanjohnson320 <nathanjohnson320@gmail.com>
2013-09-25 22:42:40 +0000

    Added code that will auto detect node updates and compile them

--- scripts/auto_update.sh
+++ scripts/auto_update.sh
@@ -16,10 +16,25 @@ else
   echo "$file1 and $file2 differ"
 
   # Get the most recent version of node
-  url=http://nodejs.org/dist/latest/
-  url+="abc"
-  wget $url
+  url=http://nodejs.org/dist/latest/node-
+  version=$(node getpage.js)
+  url+=$version.tar.gz
+  wget $url -O ./node/node.source.tar.gz
+
+  # change into the node directory and untar
+  cd ./node
+  tar -xvzf node.source.tar.gz
+  rm node.source.tar.gz
+
+  # Configure and make the file
+  cd ./node-$version
+  ./configure
+  make
+
+  # Install and make a package
+  sudo checkinstall -D -pkgversion ${version#?} -y
 
   # Rename the new changelog to the old one
+  cd ../../
   mv ./new-log.html changelog.html 
 fi
