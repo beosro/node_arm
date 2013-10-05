afa07858321cf152298533054cb64f7ecdaf285a
nathanjohnson320 <nathanjohnson320@gmail.com>
2013-10-05 21:41:29 +0000

    auto_update now runs on cron tab

--- web.js
+++ web.js
@@ -5,15 +5,12 @@ var server = http.createServer(app).listen(process.env.PORT || 9090);
 console.log("Listening on port " + (process.env.PORT || "9090"));
 
 // Routes
-app.get('/node_latest_armhf.deb', function (req, res) {
-  res.download(__dirname + "/files/node_0.10.20-1_armhf.deb");
-});
-
 app.get('/node_0.10.18-1_armhf.deb', function (req, res) {
   res.download(__dirname + "/files/node_0.10.18-1_armhf.deb");
 });
-
 app.get('/node_0.10.17-1_armhf.deb', function (req, res) {
   res.download(__dirname + "/files/node_0.10.17-1_armhf.deb");
 });
-
+app.get('/node_latest_armhf.deb', function (req, res) {
+  res.download(__dirname + "/files/node_0.10.20-1_armhf.deb");
+});
