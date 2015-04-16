d8e204a8b388e45ed06c5c4070e3dc9aa2832ca1
Nathan Johnson <najohnson@redventures.com>
2015-04-15 22:40:02 -0400

    Got an email from mongolabs telling me to stop sucking

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.get('/', function(req, res) {
       collection.mapReduce(map, reduce, {out : {inline: 1}}, function(err, results) {
         collection.find().sort({"version": -1}).toArray(function(err, rs) {
           var total = results.length > 0 ? results[0].value : "Over 9,000";
+          db.close();
           res.render('index', { title: 'node-arm', total: total, versions: rs });
         });
       });
@@ -31,6 +32,7 @@ router.get('/node_0.10.35_armhf.deb', function (req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
     db.collection('downloads', function(err, collection) {
       collection.update({ "version": "0.10.35" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err, rs){
+        db.close();
         res.download(__dirname + '/files/node_0.10.35-1_armhf.deb');
       });
     });
@@ -41,6 +43,7 @@ router.get('/node_0.10.36_armhf.deb', function (req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
     db.collection('downloads', function(err, collection) {
       collection.update({ "version": "0.10.36" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err,rs) {
+        db.close();
         res.download(__dirname + '/files/node_0.10.36-1_armhf.deb');
       });
     });
@@ -51,6 +54,7 @@ router.get('/node_latest_armhf.deb', function (req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
     db.collection('downloads', function(err, collection) {
       collection.update({ "version": "0.12.1" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err,rs) {
+        db.close();
         res.download(__dirname + '/files/node_0.12.1-1_armhf.deb', 'node_latest_armhf.deb');
       });
     });
@@ -61,6 +65,7 @@ router.get('/node_latest_unstable_armhf.deb', function (req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
     db.collection('downloads', function(err, collection) {
       collection.update({ "version": "0.0.1" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err,rs) {
+        db.close();
         res.download(__dirname + '/files/node_0.11.16-1_armhf.deb', 'node_latest_unstable_armhf.deb');
       });
     });
