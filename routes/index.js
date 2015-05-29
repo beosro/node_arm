0a057948e80555916bf7ee13e0e8827481055cc6
Nathan Johnson <najohnson@redventures.com>
2015-05-29 17:52:09 -0400

    Added safeguards for when mongolabs sucks and crashes

--- routes/index.js
+++ routes/index.js
@@ -7,71 +7,79 @@ var mongoUri = process.env.MONGOLAB_URI ||
 
 router.get('/', function(req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('downloads', function(err, collection) {
-      var map = function() { emit("downloads", this.downloads); };
-      var reduce = function(key, values) {
-        var i, sum = 0;
-        for (i in values) {
-          sum += values[i];
-        }
-        return sum;
-      };
-      collection.mapReduce(map, reduce, {out : {inline: 1}}, function(err, results) {
-        collection.find().sort({"version": -1}).toArray(function(err, rs) {
-          var total = results.length > 0 ? results[0].value : "Over 9,000";
-          db.close();
-          res.render('index', { title: 'node-arm', total: total, versions: rs });
+    if (typeof db !== 'undefined') {
+      db.collection('downloads', function(err, collection) {
+        var map = function() { emit("downloads", this.downloads); };
+        var reduce = function(key, values) {
+          var i, sum = 0;
+          for (i in values) {
+            sum += values[i];
+          }
+          return sum;
+        };
+        collection.mapReduce(map, reduce, {out : {inline: 1}}, function(err, results) {
+          collection.find().sort({"version": -1}).toArray(function(err, rs) {
+            var total = results.length > 0 ? results[0].value : "Over 9,000";
+            db.close();
+            res.render('index', { title: 'node-arm', total: total, versions: rs });
+          });
         });
       });
-    });
+    }
   });
 
 });
 
 router.get('/node_0.10.35_armhf.deb', function (req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('downloads', function(err, collection) {
-      collection.update({ "version": "0.10.35" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err, rs){
-        db.close();
-        res.download(__dirname + '/files/node_0.10.35-1_armhf.deb');
+    if (typeof db !== 'undefined') {
+      db.collection('downloads', function(err, collection) {
+        collection.update({ "version": "0.10.35" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err, rs){
+          db.close();
+          res.download(__dirname + '/files/node_0.10.35-1_armhf.deb');
+        });
       });
-    });
+    }
   });
 });
 
 router.get('/node_0.10.36_armhf.deb', function (req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('downloads', function(err, collection) {
-      collection.update({ "version": "0.10.36" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err,rs) {
-        db.close();
-        res.download(__dirname + '/files/node_0.10.36-1_armhf.deb');
+    if (typeof db !== 'undefined') {
+      db.collection('downloads', function(err, collection) {
+        collection.update({ "version": "0.10.36" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err,rs) {
+          db.close();
+          res.download(__dirname + '/files/node_0.10.36-1_armhf.deb');
+        });
       });
-    });
+    }
   });
 });
 
 router.get('/node_latest_armhf.deb', function (req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('downloads', function(err, collection) {
-      collection.update({ "version": "0.12.1" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err,rs) {
-        db.close();
-        res.download(__dirname + '/files/node_0.12.1-1_armhf.deb', 'node_latest_armhf.deb');
+    if (typeof db !== 'undefined') {
+      db.collection('downloads', function(err, collection) {
+        collection.update({ "version": "0.12.1" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err,rs) {
+          db.close();
+          res.download(__dirname + '/files/node_0.12.1-1_armhf.deb', 'node_latest_armhf.deb');
+        });
       });
-    });
+    }
   });
 });
 
 router.get('/node_latest_unstable_armhf.deb', function (req, res) {
   mongo.Db.connect(mongoUri, function (err, db) {
-    db.collection('downloads', function(err, collection) {
-      collection.update({ "version": "0.0.1" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err,rs) {
-        db.close();
-        res.download(__dirname + '/files/node_0.11.16-1_armhf.deb', 'node_latest_unstable_armhf.deb');
+    if (typeof db !== 'undefined') {
+      db.collection('downloads', function(err, collection) {
+        collection.update({ "version": "0.0.1" }, { $inc: { "downloads": 1 } }, { upsert: true }, function(err,rs) {
+          db.close();
+          res.download(__dirname + '/files/node_0.11.16-1_armhf.deb', 'node_latest_unstable_armhf.deb');
+        });
       });
-    });
+    }
   });
 });
-
-
 
 module.exports = router;
